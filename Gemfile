6dc06ab06aed78cf16d110e8124c8c7fc38d5e3c
Your Username <you@example.com>
2021-01-12 22:06:01 -0500

    added bcrypt gem and added has_secure_password and validations to the Groomer model

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
+gem 'bcrypt'
 gem 'pry'
 gem "simple_calendar", "~> 2.0"
 
--- app/models/groomer.rb
+++ app/models/groomer.rb
@@ -1,4 +1,7 @@
 class Groomer < ApplicationRecord
+    has_secure_password
     has_many :appointments
     has_many :pets, through: :appointments
+    validates :username, presence: true
+    validates :password, presence: true
 end
