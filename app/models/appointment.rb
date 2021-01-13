1390c9a534638d7f84d26dd3e0fe8d049e1a6c5e
Your Username <you@example.com>
2021-01-13 15:52:35 -0500

    added start_time method to the Appointment model and the calendar shows the appointment_time

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,8 +5,14 @@ class Appointment < ApplicationRecord
     def pet_name=(name)
         self.pet = Pet.find_or_create_by(name: name)
     end
-    
+
     def pet_name
         self.pet ? self.pet.name : nil
     end
+
+    def start_time
+        self.appointment_time ##Where 'start' is a attribute of type 'Date' accessible through MyModel's relationship
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :owners
   
-  resources :groomers do 
-    resources :appointments
-  end
-  
-  resources :pets
+  # resources :groomers do 
+  #   resources :appointments
+  # end
+
+  resources :groomers
+  resources :appointments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
